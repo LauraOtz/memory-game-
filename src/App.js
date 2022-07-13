466f696263b0a9fad8579775d90538f29ac5956c
Laura Ortiz <bjtamyl@hotmail.com>
2022-07-13 13:10:16 -0300

    primer actualizacion

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Background from './components/Background';
+
+const BASE_URL= 'https://api.pexels.com/v1/search';
+function App() {
+
+    fetch (BASE_URL,{headers:{Authorization: process.env.REACT_APP_AUTH_KEY,},});
+
+
+    return (
+        <>
+            <Background />
+            <h1>Memory Game</h1>
+        </>
+    );
+}
+
+export default App;
